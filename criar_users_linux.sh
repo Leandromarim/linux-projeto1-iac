e69c7ebf08d5d81d1cc10110d1aa761e02998018
Leandro Marim <60162892+Leandromarim@users.noreply.github.com>
2023-05-07 02:48:29 -0300

    Update criar_users_linux.sh
    Fiz do zero acompanhando o video, porém para não dar plágio eu alterei novamente os nomes dos envolvidos.

--- criar_users_linux.sh
+++ criar_users_linux.sh
@@ -16,17 +16,17 @@ groupadd GRP_SEC
 
 echo "Criando usuarios..."
 
-useradd carlos -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_ADM
-useradd maria -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_ADM
-useradd joao -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_ADM
+useradd leandro -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_ADM
+useradd aparecida -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_ADM
+useradd deusina -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_ADM
 
-useradd debora -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_VEN
-useradd sebastiana -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_VEN
-useradd roberto -m -s /bin/bash -p $ (openssl passdw -crypt Senha123) -G GRP_VEN
+useradd victor -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_VEN
+useradd kaleb -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_VEN
+useradd jose -m -s /bin/bash -p $ (openssl passdw -crypt Senha123) -G GRP_VEN
 
-useradd josefina -m -s /bin/bash -p $ (openssl passdw -crypt Senha123) -G GRP_SEC
-useradd amanda -m -s /bin/bash -p $ (openssl passdw -crypt Senha123) -G GRP_SEC
-useradd rogerio -m -s /bin/bash -p $ (openssl passdw -crypt Senha123) -G GRP_SEC
+useradd ambrosina -m -s /bin/bash -p $ (openssl passdw -crypt Senha123) -G GRP_SEC
+useradd rodrigo -m -s /bin/bash -p $ (openssl passdw -crypt Senha123) -G GRP_SEC
+useradd eduardo -m -s /bin/bash -p $ (openssl passdw -crypt Senha123) -G GRP_SEC
 
 echo "Especificando permissoes dos diretorios..."
 
